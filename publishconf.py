94c15f93dcb45761757cdb4de9be3ffdd9f9360f
Alex Reece <awreece@gmail.com>
2013-04-13 21:32:57 -0400

    add disqus

--- publishconf.py
+++ publishconf.py
@@ -14,5 +14,5 @@ DELETE_OUTPUT_DIRECTORY = True
 # Uncomment following line for absolute URLs in production:
 #RELATIVE_URLS = False
 
-#DISQUS_SITENAME = ""
+DISQUS_SITENAME = "codearcana"
 GOOGLE_ANALYTICS = "UA-40107691-1"
